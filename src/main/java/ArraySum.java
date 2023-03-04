ac0150d708f30d4656c2f7f02c99f11d627def62
Zach Wilcox <Zacdwil@hotmail.com>
2023-03-04 06:26:11 +0000

    commit

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,10 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int total = 0;
+        for(int val : arr){
+            total += val;
+        }
+        return total;
     }
 }
